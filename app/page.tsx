76950f8afe5d533581d36bffed56da8706136f91
Shinta Apriyani <ap.shinta18@gmail.com>
2025-07-08 19:18:42 +0700

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -12,19 +12,10 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
+        <h1 className="text-4xl font-bold text-purple-600">Hello World 🌟</h1>
+          <p className="text-lg text-gray-700">
+            Ini adalah tugas pertama kelas Fullstack Developer.
+          </p>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
