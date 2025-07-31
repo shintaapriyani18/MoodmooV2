22ead9f0d4af707a09b72e204d0cf546c2e7078a
Shinta Apriyani <ap.shinta18@gmail.com>
2025-07-31 20:24:37 +0700

    Fix bug api products part 2

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -1,24 +1,36 @@
 import { prisma } from '@/lib/prisma';
 import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import type { NextApiRequest } from 'next';
 
-export async function GET(_req: Request, { params }: { params: { id: string } }) {
+type Params = {
+  params: {
+    id: string;
+  };
+};
+
+export async function GET(_req: NextRequest, { params }: Params) {
   const id = Number(params.id);
-
-  if (!id) return NextResponse.json({ error: 'Invalid ID' }, { status: 400 });
+  if (isNaN(id)) {
+    return NextResponse.json({ error: 'Invalid ID' }, { status: 400 });
+  }
 
   const product = await prisma.product.findUnique({
     where: { id },
   });
 
-  if (!product) return NextResponse.json({ error: 'Not found' }, { status: 404 });
+  if (!product) {
+    return NextResponse.json({ error: 'Not found' }, { status: 404 });
+  }
 
   return NextResponse.json(product);
 }
 
-export async function PUT(req: Request, { params }: { params: { id: string } }) {
+export async function PUT(req: NextRequest, { params }: Params) {
   const id = Number(params.id);
-
-  if (!id) return NextResponse.json({ error: 'Invalid ID' }, { status: 400 });
+  if (isNaN(id)) {
+    return NextResponse.json({ error: 'Invalid ID' }, { status: 400 });
+  }
 
   const { name, price, desc } = await req.json();
 
@@ -30,10 +42,11 @@ export async function PUT(req: Request, { params }: { params: { id: string } }) 
   return NextResponse.json(updated);
 }
 
-export async function DELETE(_req: Request, { params }: { params: { id: string } }) {
+export async function DELETE(_req: NextRequest, { params }: Params) {
   const id = Number(params.id);
-
-  if (!id) return NextResponse.json({ error: 'Invalid ID' }, { status: 400 });
+  if (isNaN(id)) {
+    return NextResponse.json({ error: 'Invalid ID' }, { status: 400 });
+  }
 
   const deleted = await prisma.product.delete({
     where: { id },
--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,13 +1,14 @@
 import { prisma } from '@/lib/prisma';
 import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
-export async function GET() {
+export async function GET(_req: NextRequest) {
   const products = await prisma.product.findMany();
   return NextResponse.json(products);
 }
 
-export async function POST(request: Request) {
-  const { name, price, desc } = await request.json();
+export async function POST(req: NextRequest) {
+  const { name, price, desc } = await req.json();
   const product = await prisma.product.create({ data: { name, price, desc } });
   return NextResponse.json(product);
 }
